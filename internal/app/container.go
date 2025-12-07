ca948c727e8e232cf287f916a79f1d648209e026
R4yL <luca.ray@protonmail.ch>
2025-12-07 13:35:15 +0100

    add dependency injection

---
+++ internal/app/container.go
@@ -0,0 +1,39 @@
+package app
+
+import (
+	"github.com/R4yL-dev/pkmc/internal/config"
+	"github.com/R4yL-dev/pkmc/internal/database"
+	"github.com/R4yL-dev/pkmc/internal/repository"
+	"github.com/R4yL-dev/pkmc/internal/service"
+	"gorm.io/gorm"
+)
+
+type Container struct {
+	DB  *gorm.DB
+	UoW repository.UnitOfWork
+
+	ItemService service.ItemService
+}
+
+func NewContainer() (*Container, error) {
+	cfg := config.Get()
+
+	db, err := database.InitDB(cfg.GetDBPath())
+	if err != nil {
+		return nil, err
+	}
+
+	uow := repository.NewUnitOfWork(db)
+
+	itemService := service.NewItemService(uow)
+
+	return &Container{
+		DB:          db,
+		UoW:         uow,
+		ItemService: itemService,
+	}, nil
+}
+
+func (c *Container) Close() error {
+	return database.CloseDB()
+}
