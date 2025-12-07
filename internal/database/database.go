d9db9a47b07a941d253a242e787c4eb65384bf30
R4yL <luca.ray@protonmail.ch>
2025-12-07 19:41:31 +0100

    silence GROM logger

--- internal/database/database.go
+++ internal/database/database.go
@@ -3,10 +3,13 @@ package database
 import (
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func InitDB(dbPath string) (*gorm.DB, error) {
-	db, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Silent),
+	})
 	if err != nil {
 		return nil, err
 	}
