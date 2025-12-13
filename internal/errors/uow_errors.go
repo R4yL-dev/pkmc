2a81442e9ae825adb515f62baf5356c6c61f2817
R4yL <luca.ray@protonmail.ch>
2025-12-13 14:20:00 +0100

    Fix redondancy Op and Operation

--- internal/errors/uow_errors.go
+++ internal/errors/uow_errors.go
@@ -7,11 +7,10 @@ import (
 
 type UOWError struct {
 	*BaseError
-	Operation string
 }
 
 func (e UOWError) Error() string {
-	return fmt.Sprintf("unit of work %s failed: %s", e.Operation, e.BaseError.Error())
+	return fmt.Sprintf("unit of work %s failed: %s", e.Op, e.BaseError.Error())
 }
 
 var (
@@ -19,9 +18,8 @@ var (
 	ErrUOWCommitFailed = errors.New("unit of work commit failed")
 )
 
-func NewUOWError(operation string, cause error) *UOWError {
+func NewUOWError(op string, cause error) *UOWError {
 	return &UOWError{
-		BaseError: NewBaseError(operation, "unit_of_work", "", cause),
-		Operation: operation,
+		BaseError: NewBaseError(op, "unit_of_work", "", cause),
 	}
 }
